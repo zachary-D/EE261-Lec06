693683ea9af32a7c9978324f271ab215fab40d40
zachary-D <zacharyd1@live.com>
2017-09-13 12:59:51 -0400

    Tweaked code to validate that the right angle == 90*, added file support
    The right angles are validated to make sure the triangle calculated is a right triangle

--- Lecture06/Lecture06.cpp
+++ Lecture06/Lecture06.cpp
@@ -1,6 +1,9 @@
+#define PI 3.14159265
+
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <fstream>
 #include <math.h>
 #include <cmath>
 #include <vector>
@@ -9,9 +12,21 @@ using std::string;
 using std::cout;
 using std::endl;
 
+using std::ofstream;
+
 //Note: General nomenclature: a and b are the horizontal and vertical legs of the triangle (and form the 90* angle), and h is the hypotenuse
 
 int P;	//The perimeter of the triangle (defined later)
+
+double toRadians(double degrees)
+{
+	return degrees * PI / 180;
+}
+
+double toDegrees(double radians)
+{
+	return radians * 180 / PI;
+}
 
 string toString(int input)		//Convert 'input' to a string and return it
 {
@@ -48,24 +63,33 @@ struct rightTriangle
 
 	bool isValid()		//Validates the values of the triangle (no negative sides, perimter adds properly, etc.)
 	{
-		return (a + b + h == P) && (a > 0) && (b > 0) && (h > 0);
+		return (a + b + h == P) &&	//Perimiter = the sum of parts
+			(a > 0) &&			//check that the 3 sides are positive
+			(b > 0) &&
+			(h > 0) &&
+			(int)((double)180 - toDegrees(abs(asin((double)b / (double)h)) + abs(acos((double)a / (double)h)))) == 90;
 	}
 
-	string print(bool endLine = true)	//Writes the triangle to the console in text form, as well as returning the data written as a string.  'endLine' determines if endl will be added to the end of the output string
+	string print(ofstream &fileOut, bool endLine = true)	//Writes the triangle to the console in text form, as well as returning the data written as a string.  'endLine' determines if endl will be added to the end of the output string
 	{
 		string output = "a:" + toString(a) + " b:" + toString(b) + " c:" + toString(h);
 		cout << output;
-		if (endLine) cout << endl;
+		fileOut << output;
+		if (endLine) cout << endl; fileOut << endl;
 		return output;
 	}
 };
 
-std::vector<rightTriangle> findTriangles(int perimiter)
+std::vector<rightTriangle> findTriangles(int perimiter)		//Searches for triangles given 'perimiter'.  Saves triangles validated to 'fileOut'
 {
 	std::vector<rightTriangle> triangles;
 
 	P = perimiter;
 	cout << "Perimiter:" << P << endl;
+	
+	ofstream fileOut;
+	fileOut.open("trianglesFound_P" + toString(perimiter) + ".txt");
+
 	//I is the length of the side the other sides are derived from
 	for (int i = 1; i < P; i++)		//Loops over i, increasing by 1 each time, until I>=P
 	{
@@ -74,7 +98,7 @@ std::vector<rightTriangle> findTriangles(int perimiter)
 		if (tri.isValid())
 		{
 			triangles.push_back(tri);	//If it is valid, add it to the vector
-			tri.print();		//And write the info to the console
+			tri.print(fileOut);		//And write the info to the console
 		}
 		else
 		{
@@ -82,12 +106,15 @@ std::vector<rightTriangle> findTriangles(int perimiter)
 
 			//Print out a statement as the triangle is invalid
 
-			/*cout << "err:";
+			/*
+			cout << "err:";
 			tri.print(false);
-			cout << " P:" << (tri.a + tri.b + tri.h) << endl;*/
+			cout << " P:" << (tri.a + tri.b + tri.h)
+				<< " angle:" << ((double)180 - toDegrees(abs(asin((double)tri.b / (double)tri.h)) + abs(acos((double)tri.a / (double)tri.h)))) << endl;*/
 		}
 	}
 	cout << endl << endl;	//Make some space on the console
+	fileOut.close();
 	return triangles;
 }
 
@@ -98,7 +125,7 @@ int main()
 	std::vector<rightTriangle> triangles_P90000 = findTriangles(90000);		//Valid triangles with a perimiter of 90000
 	
 	
-
+	cout << "Calculations complete!" << endl;
 
 	std::cin.get();
 
